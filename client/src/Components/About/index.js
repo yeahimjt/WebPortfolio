95f592223ef796205fe8708f5626637110c633af
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2022-10-18 06:08:04 -0500

    Tweaking about me layout for 3d object later

--- client/src/Components/About/index.js
+++ client/src/Components/About/index.js
@@ -1,20 +1,26 @@
 import './index.scss'
-
+import Navbar from '../Nav'
+import aboutme_img from '../../Assets/aboutme.jpg'
 const About = () =>  {
     return (
         <>
-            <div className="about-container">
-                <h2 className="about-heading">Jonathan Andrew Trevino</h2>
-                <h5 className="about-subheading">Full Stack Software Developer/Engineer</h5>
-                <p className="about-text"> Passionate about learning about anything I can get my 
-                    hands on. I enjoy being able to express my creativity with projects 
-                    I set in front of me and seeing the final outcome. I am aspiring to
-                    obtain an Entry-Level job in the Software Engineering domain. 
-                    I strive in both  team based projects as well as 
-                    single-handled projects. 
-                </p>
-            </div> 
-
+            <div id="about">
+                <Navbar></Navbar>
+                <div className="about-container">
+                    <img className="aboutme_img" src={aboutme_img} alt="About me"></img>
+                    <h2 className="about-heading">Jonathan Andrew Trevino</h2>
+                    <h5 className="about-subheading">Full Stack Software Developer/Engineer</h5>
+                    <p className="about-text">Passionate about Full stack development, and the knowledge that can be absorbed with working with all aspects that come with software development.
+                        I enjoy being able to express my creativity with the projects I set
+                        in front of myself and being able to see the final outcome. Passionate about Full stack development, and the knowledge that can be absorbed with working with all aspects that come with software development.
+                        I enjoy being able to express my creativity with the projects I set
+                        in front of myself and being able to see the final outcome. Passionate about Full stack development, and the knowledge that can be absorbed with working with all aspects that come with software development.
+                        I enjoy being able to express my creativity with the projects I set
+                        in front of myself and being able to see the final outcome. 
+                    </p>
+                </div> 
+                
+            </div>
         </>
     )
 
