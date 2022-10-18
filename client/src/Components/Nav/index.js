09c0352c07c30d3b2ee1dead10444ed043897183
yeahimjt <80553412+yeahimjt@users.noreply.github.com>
2022-10-18 06:09:38 -0500

    Red nav color, increased letter spacing

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
-import {Link, NavLink} from 'react-router-dom'
+
+import React, {useState} from "react"
+import {Link, NavLink, useLocation} from 'react-router-dom'
 import Logo from '../../Assets/logo-2.svg'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faHome} from '@fortawesome/free-solid-svg-icons'
@@ -11,49 +13,78 @@ import {FaGithubSquare} from 'react-icons/fa'
 import {BsLinkedin} from 'react-icons/bs'
 
 
-const Navbar = () => (
+const Navbar = () => {
+
+    const [aTag, setATag] = React.useState("");
+
+    const location = useLocation();
+    React.useEffect(() => {
+      if (window.location.pathname === "/about") {
+
+
+      } else {
+
+      }
+    }, [location]);
+    
+    return (
+
     <div className="nav-bar">
         <Link className='logo' to='/'>
             </Link>
         <nav>
-            <ul className="h">
+            <ul>
                 <li>
-                    <NavLink exact="true" activeclassname="active" className="about" to="/">
-                        <FontAwesomeIcon icon={faHome} className="about-icon"></FontAwesomeIcon>
+                    <div className="nav-item">
+                    <NavLink exact="true" activeclassname="activated" className="about" to="/">
+                        <p style={{color: aTag}}>Home</p>
                     </NavLink>
+                    </div>
                 </li>
                 <li>
+                    <div className="nav-item">  
                     <NavLink exact="true" activeclassname="active" className="person" to="/about">
-                        <FontAwesomeIcon icon={faUser} className="person-icon"></FontAwesomeIcon>
+                        <p className="nav-item" style={{color: aTag}}>About me</p>
                     </NavLink>
+
+                    </div>
                 </li>
                 <li>
+                    <div className="nav-item">
                     <NavLink exact="true" activeclassname="active" className="project" to="/projects">
-                    <FontAwesomeIcon icon={faFolder} className="project-icon"></FontAwesomeIcon>
+                        <p className="nav-item" style={{color: aTag}}>Projects</p>
                     </NavLink>
+
+                    </div>
                 </li>
                 <li>
+                    <div className="nav-item">
                     <NavLink exact="true" activeclassname="active" className="demo" to="/demos">
-                    <FontAwesomeIcon icon={faLaptopCode} className="demo-icon"></FontAwesomeIcon>
+                        <p className="nav-item" style={{color: aTag}}>Demo</p>
                     </NavLink>
+
+                    </div>
                 </li>
                 <li>
+                    <div className="nav-item">
                     <NavLink exact="true" activeclassname="active" className="feed" to="/feedback">
-                        <FontAwesomeIcon icon={faStar} className="feedback-icon"></FontAwesomeIcon>
+                        <p className="nav-item" style={{color: aTag}}>Feedback</p>
                     </NavLink>
+                    </div>
                 </li>
             </ul>
             
         </nav>
         <ul>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/jonathanandrewtrevino"><BsLinkedin className="linkedin-icon"/></a>
+                    <a  style={{color:aTag}} target="_blank" rel="noreferrer" href="https://linkedin.com/in/jonathanandrewtrevino"><BsLinkedin className="linkedin-icon"/></a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://github.com/yeahimjt?tab=repositories"><FaGithubSquare className="github-icon"/></a>
+                    <a style={{color:aTag}} target="_blank" rel="noreferrer" href="https://github.com/yeahimjt?tab=repositories"><FaGithubSquare className="github-icon"/></a>
                 </li>
             </ul>
     </div>
-)
+    );
+}
 
 export default Navbar;
